6259bb972e4879a9275265e48077617339501c8e
adhitamapra <adhitama52@yahoo.com>
2019-11-21 09:53:38 +0700

    “HaloDunia

---
+++ precache-manifest.1699b2b1432fd2d3c1df2cf425384bef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7b4525433d943e8da4c",
+    "url": "/css/chunk-vendors.646823c3.css"
+  },
+  {
+    "revision": "de1540738f0532f5fffca1a3ee9038cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e452f660a6978a093449",
+    "url": "/js/app.0dc325df.js"
+  },
+  {
+    "revision": "f7b4525433d943e8da4c",
+    "url": "/js/chunk-vendors.acc6f17c.js"
+  },
+  {
+    "revision": "2fa781c807d5582bb740b24ad158fc66",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
